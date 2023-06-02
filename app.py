ea96574be263d454d6fb9182958452e8cb989eea
Tweetor Official <133514173+TweetorOfficial@users.noreply.github.com>
2023-06-02 13:29:04 +0000

    Hopefully no more errors

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ DATABASE = "tweetor.db"
 sqlite3.connect(DATABASE).cursor().execute(
     """
     CREATE TABLE IF NOT EXISTS tweets  (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    userHandle TEXT NOT NULL,
-    username TEXT NOT NULL,
-    hashtag TEXT NOT NULL
-)
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        userHandle TEXT NOT NULL,
+        username TEXT NOT NULL,
+        hashtag TEXT NOT NULL
+    )
 """)
 
 sqlite3.connect(DATABASE).cursor().execute(
@@ -36,7 +36,7 @@ sqlite3.connect(DATABASE).cursor().execute(
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL,
-        turbo INTEGER,
+        turbo INTEGER DEFAULT 0,
         handle TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL
     )
@@ -77,7 +77,6 @@ def home() -> Response:
         cursor = db.cursor()
         cursor.execute("SELECT turbo FROM users WHERE handle = ?", (session["handle"], ))
         if cursor.fetchone()["turbo"]==1:
-            print("turbo")
             return render_template("home.html", tweets=tweets, loggedIn=("username" in session), turbo=True)
         return render_template("home.html", tweets=tweets, loggedIn=("username" in session), turbo=False)
     return render_template("home.html", tweets=tweets, loggedIn=("username" in session), nitro=False)
@@ -88,40 +87,33 @@ def submit_tweet() -> Response:
     print(request.form)
     content = request.form["content"]
     if len(content) > 10000:
-        return redirect("/")
+        return render_template("error.html", error="Message was too long.")
     if "username" not in session:
-        return redirect("/")
+        return render_template("error.html", error="You are not logged in.")
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT turbo FROM users WHERE handle = ?", (session["handle"], ))
-    if cursor.fetchone()["turbo"]==0 and (len(content)>280 or "*" in content):
-        return redirect("/")
+    if cursor.fetchone()["turbo"]==0 and (len(content)>280 or "*" in content or "_" in content):
+        return render_template("error.html", error="You do not have tweetor turbo.")
     print(session)
     hashtag = request.form["hashtag"]
-    cursor = db.cursor()
-    cursor.execute(
-        "INSERT INTO tweets (content, userHandle, username, hashtag) VALUES (?, ?, ?, ?)",
-        (
-            content,
-            session["handle"],
-            session["username"],
-            hashtag,
-        ),
-    )
-    def tweet() -> Response:
-        if "username" not in session:
-            return redirect("/signup")
+    if "username" not in session:
+        return redirect("/signup")
 
     tweet_content = request.form["content"]
 
-    if is_profanity(tweet_content):
-        return Response("Profanity is not allowed.", status=400)
+    if is_profanity(tweet_content+hashtag) != []:
+        for word in is_profanity(tweet_content+hashtag):
+            print(word)
+            tweet_content = tweet_content.replace(word[0], word[1])
+            hashtag = hashtag.replace(word[0], word[1])
+
+    if "ur mom" in tweet_content:
+        return render_template("error.html", error="Message contained ur mom tweetor servers were overloaded. Could not handle tweet.")
 
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO tweets (username, content) VALUES (?, ?)", (session["username"], tweet_content))
-    db.commit()
-    return redirect("/")
+    cursor.execute("INSERT INTO tweets (username, content, userHandle, hashtag) VALUES (?, ?, ?, ?)", (session["username"], tweet_content, session["handle"], hashtag, ))
     db.commit()
     return redirect(url_for("home"))
 
@@ -187,32 +179,32 @@ def login() -> Response:
     return render_template("login.html")
 
 
-@app.route('/user/<username>')
-def user_profile(username: str) -> Response:
- conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-    user = cursor.fetchone()
-    if user:
-        cursor.execute("SELECT followers FROM users WHERE username = ?", (username,))
-        follower_count = cursor.fetchone()[0]
-    else:
-        return redirect("/signup")
-    return render_template("user.html", user=user, followerCount=follower_count, loggedIn=("username" in session))
-    # Get the user's information from the database
-    c.execute("SELECT * FROM users WHERE handle=?", (username,))
-    user = c.fetchone()
+# @app.route('/user/<username>')
+# def user_profile(username: str) -> Response:
+#     conn = get_db()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+#     user = cursor.fetchone()
+#     if user:
+#         cursor.execute("SELECT followers FROM users WHERE username = ?", (username,))
+#         follower_count = cursor.fetchone()[0]
+#     else:
+#         return redirect("/signup")
+#     return render_template("user.html", user=user, followerCount=follower_count, loggedIn=("username" in session))
+#     # Get the user's information from the database
+#     c.execute("SELECT * FROM users WHERE handle=?", (username,))
+#     user = c.fetchone()
 
-    if user:
-        # Get the user's tweets from the database
-        c.execute("SELECT * FROM tweets WHERE userHandle=?", (username,))
-        tweets = c.fetchall()
+#     if user:
+#         # Get the user's tweets from the database
+#         c.execute("SELECT * FROM tweets WHERE userHandle=?", (username,))
+#         tweets = c.fetchall()
 
-        # Render the template with the user's information and tweets
-        return render_template("user.html", user=user, tweets=tweets, loggedIn=("username" in session))
+#         # Render the template with the user's information and tweets
+#         return render_template("user.html", user=user, tweets=tweets, loggedIn=("username" in session))
 
-    # If the user doesn't exist, display an error message
-    return "User not found"
+#     # If the user doesn't exist, display an error message
+#     return "User not found"
 
 @app.route('/tweets/<tweet_id>')
 def singleTweet(tweet_id: str) -> Response:
@@ -289,65 +281,17 @@ def logout() -> Response:
         session.pop('handle', None)
         session.pop('username', None)
     return redirect("/")
+
 # Profanity filter
-profanity_words = ["fuck", "badword", "curse"]
+profanity_words = [("arse", "butt"), ("arsehead", "butt"), ("arsehole", "butt"), ("ass", "butt"), ("asshole", "butt"), ("bastard", "******"), ("bitch", "******"), ("bloody", "******"), ("bollocks", "******"), ("bugger", "bug"), ("bullshit", "cow poop"), ("bs", "cow poop"), ("crap", "treasure"), ("cunt", "******"), ("damn", "aadam nason"), ("dick", "detective"), ("dyke", "********"), ("frigger", "69"), ("frick", "69"), ("fuck", "69"), ("hell", "heaven"), ("kike", "******"), ("nigra", "******"), ("nigga", "******"), ("piss", "******"), ("prick", "******"), ("shit", "poo"), ("slut", "******"), ("son of a", "******"), ("spastic", "poo"), ("turd", "poop"), ("twat", "nonoword"), ("wanker", "that's illegal")]
 
-def is_profanity(text: str) -> bool:
+def is_profanity(text: str):
+    words = []
     for word in profanity_words:
-        if word in text.lower():
-            return True
-    return False
-
-# Helper function to count followers
-def get_follower_count(username: str) -> int:
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT followers FROM users WHERE username = ?", (username,))
-    follower_count = cursor.fetchone()
-    if follower_count:
-        return follower_count[0]
-    return 0
-
-# Helper function to count likes for a tweet
-def get_like_count(tweet_id: int) -> int:
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT likes FROM tweets WHERE id = ?", (tweet_id,))
-    like_count = cursor.fetchone()
-    if like_count:
-        return like_count[0]
-    return 0
-
-@app.route("/follow/<username>")
-def follow(username: str) -> Response:
-    if "username" not in session:
-        return redirect("/signup")
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT followers FROM users WHERE username = ?", (username,))
-    follower_count = cursor.fetchone()
-    if follower_count:
-        follower_count = follower_count[0]
-    else:
-        follower_count = 0
-    db.commit()
-    if follower_count >= session["followers"]:
-        return redirect("/")
-    cursor.execute("UPDATE users SET followers = followers + 1 WHERE username = ?", (username,))
-    db.commit()
-    return redirect(url_for("user", username=username))
-
-@app.route("/like/<int:tweet_id>")
-def like(tweet_id: int) -> Response:
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM tweets WHERE id = ?", (tweet_id,))
-    tweet = cursor.fetchone()
-    if tweet:
-        cursor.execute("UPDATE tweets SET likes = likes + 1 WHERE id = ?", (tweet_id,))
-        db.commit()
-        return redirect(url_for("home"))
-    return redirect("/")
+        if word[0] in text.lower():
+            print(word)
+            words.append((word[0], word[1]))
+    return words
 
 @app.route("/user/<username>")
 def user_profile(username: str) -> Response:
@@ -355,12 +299,11 @@ def user_profile(username: str) -> Response:
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
     user = cursor.fetchone()
-    if user:
-        cursor.execute("SELECT followers FROM users WHERE username = ?", (username,))
-        follower_count = cursor.fetchone()[0]
-    else:
-        return redirect("/signup")
-    return render_template("user.html", user=user, followerCount=follower_count, loggedIn=("username" in session))
+    if not user:
+        return redirect("/home")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tweets WHERE userHandle = ?", (username, ))
+    return render_template("user.html", user=user, loggedIn=("username" in session), tweets=cursor.fetchall())
 
 if __name__ == "__main__":
     app.run(debug=False)
